8ecb6254016c641e6eb95fdc30011b122cb2aa73
Lotte <lottetheisen+github@gmail.com>
2018-08-12 17:37:53 -0500

    CheckoutHelper boilerplate

---
+++ src/EmployeeEquipmentCheckoutSystem.Core/CheckoutHelper.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using EmployeeEquipmentCheckoutSystem.Core.Data;
+
+namespace EmployeeEquipmentCheckoutSystem.Core
+{
+    public class CheckoutHelper
+    {
+        public void Checkout(Employee Employee, ICheckable item)
+        {
+            // TODO:
+        }
+
+        public void Checkout(Employee Employee, IEnumerable<ICheckable> item)
+        {
+            // TODO:
+        }
+
+        public void CheckIn(Employee Employee, ICheckable item)
+        {
+            // TODO:
+        }
+
+        public void CheckIn(Employee Employee, IEnumerable<ICheckable> item)
+        {
+            // TODO:
+        }
+    }
+}
