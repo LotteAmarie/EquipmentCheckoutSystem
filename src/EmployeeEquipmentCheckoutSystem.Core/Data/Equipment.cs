d44c3e78b14fb80394e975caaa0966ce870694ec
Lotte <lottetheisen+github@gmail.com>
2018-08-19 18:09:02 -0500

    Changed RequestedById to RequestedByIds

--- src/EmployeeEquipmentCheckoutSystem.Core/Data/Equipment.cs
+++ src/EmployeeEquipmentCheckoutSystem.Core/Data/Equipment.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace EmployeeEquipmentCheckoutSystem.Core.Data
 {
     public class Equipment : ICheckable
@@ -8,7 +10,7 @@ namespace EmployeeEquipmentCheckoutSystem.Core.Data
         public int LastCheckedById { get; set; }
         public bool IsAvailable { get; set; }
         // TODO: Band-aid fix, look into FK relationships in EF Core
-        public int RequestedById { get; set; }
+        public List<int> RequestedById { get; set; }
         public SafetyLevel RequiredSafetyLevel { get; set; }
     }
 }
