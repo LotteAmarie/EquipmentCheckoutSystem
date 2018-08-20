f875a79204a9cecefce3053ccabfde257a57d09b
Lotte <lottetheisen+github@gmail.com>
2018-08-19 19:18:07 -0500

    Tested both authorized and unauthorized paths for IsAuthorizedFor

---
+++ test/EmployeeEquipmentCheckoutSystem.Core.Tests/EmployeeExtensionTests.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using EmployeeEquipmentCheckoutSystem.Core.Data;
+using Xunit;
+
+namespace EmployeeEquipmentCheckoutSystem.Core.Tests
+{
+    public class EmployeeExtensionTests
+    {
+        [Fact]
+        public void IsAuthorizedFor_TrueWhenSafetyLevelIsSufficient()
+        {
+            //Given
+            var employee = new Employee 
+            {
+                Id = 001,
+                CheckedItems = new List<ICheckable> { },
+                CheckedItemHistory = new List<ICheckable> { },
+                EMailAddress = "001@somewhere.com",
+                MaximumSafetyClearance = SafetyLevel.A
+            };
+
+            var equipment = new Equipment 
+            { 
+                SerialNumber = 100, 
+                Location = "Warehouse 1", 
+                LastCheckedById = 0, 
+                RequestedByIds = new List<int> { },
+                IsAvailable = true, 
+                RequiredSafetyLevel = SafetyLevel.A 
+            };
+
+            //When
+            var result = employee.IsAuthorizedFor(equipment);
+
+            //Then
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsAuthorizedFor_FalseWhenSafetyLevelIsInsufficient()
+        {
+            //Given
+            var employee = new Employee 
+            {
+                Id = 001,
+                CheckedItems = new List<ICheckable> { },
+                CheckedItemHistory = new List<ICheckable> { },
+                EMailAddress = "001@somewhere.com",
+                MaximumSafetyClearance = SafetyLevel.C
+            };
+
+            var equipment = new Equipment 
+            { 
+                SerialNumber = 100, 
+                Location = "Warehouse 1", 
+                LastCheckedById = 0, 
+                RequestedByIds = new List<int> { },
+                IsAvailable = true, 
+                RequiredSafetyLevel = SafetyLevel.A 
+            };
+
+            //When
+            var result = employee.IsAuthorizedFor(equipment);
+
+            //Then
+            Assert.False(result);
+        }
+    }
+}
